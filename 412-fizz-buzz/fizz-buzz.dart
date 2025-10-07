fcfa55945b90492b9179c45c2721af9187189ecc
sinanshanu517-rgb <sinanshanu517@gmail.com>
2025-10-07 17:17:21 +0530

    Time: 3 ms (71.43%) | Memory: 146.6 MB (97.96%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.dart
@@ -0,0 +1,18 @@
+class Solution {
+  List<String> fizzBuzz(int n) {
+    List<String>res=[];
+    for(int i = 1;i<=n; i++){
+        if(i%3==0 && i% 5==0){
+            res.add("FizzBuzz");
+        }else if (i%3==0){
+            res.add("Fizz");
+        }else if ( i % 5==0){
+            res.add("Buzz");
+        }else{
+            res.add(i.toString());
+        }
+    }
+    return res;
+  }
+
+}
