9d451b3fbbd15ffea980087273b6601d95016eae
sinanshanu517-rgb <sinanshanu517@gmail.com>
2025-10-08 11:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 152.3 MB (16.13%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.dart
@@ -0,0 +1,10 @@
+class Solution {
+  List<int> getConcatenation(List<int> nums) {
+    List<int> output =[];
+    for(int i=0;i<nums.length;i++) {
+        output.add(nums[i]);
+    } for(int i=0;i<nums.length;i++) {
+        output.add(nums[i]);
+    } return output;
+  }
+}
