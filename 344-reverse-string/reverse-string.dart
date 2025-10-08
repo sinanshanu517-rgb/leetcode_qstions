61cdb26160a71629ee22324b3a3d38da4bc55704
sinanshanu517-rgb <sinanshanu517@gmail.com>
2025-10-08 10:30:34 +0530

    Time: 0 ms (100.00%) | Memory: 150.1 MB (90.48%) - LeetSync

---
+++ 344-reverse-string/reverse-string.dart
@@ -0,0 +1,16 @@
+class Solution {
+  void reverseString(List<String> s) {
+    int left = 0;
+    int right = s.length-1;
+
+    while(left < right){
+        String temp = s[left];
+        s[left]=s[right];
+        s[right]=temp;
+
+        left++;
+        right--;
+    }
+    
+  }
+}
